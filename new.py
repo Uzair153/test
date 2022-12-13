5132af9d0dd21d5ec6644e86ea3a02d0222543a7
Qasim <Wasiq@gmail.com>
2022-12-13 06:39:24 -0800

    some new code of function display is added

--- new.py
+++ new.py
@@ -1,2 +1,4 @@
 print("This is python first program")
 print ('This is new line')
+print("Commit is successfull")
+print ('This is new line')
---
+++ test.py
@@ -0,0 +1,2 @@
+print("This is python first program")
+print ('This is new  new line')
